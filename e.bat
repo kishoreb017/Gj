a436530b7fc7eacd4b1ba2b1001eea47161722ad
ravish <ravishacr@gmail.com>
2019-11-05 16:43:06 +0530

    fg

---
+++ e.bat
@@ -0,0 +1,1 @@
+java -cp bin;s/* org.testng.TestNG testng.xml &pause
---
+++ src/qsp/Demo1.java
@@ -0,0 +1,20 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void test1()
+	{
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+	
+		driver.get("https://www.google.com");
+	
+	}
+
+}
